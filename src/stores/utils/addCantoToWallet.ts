8f20cffb882aa26b3bcce577cfedc869ba9d2869
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-09-23 14:31:17 -0400

    add network fix

--- src/stores/utils/addCantoToWallet.ts
+++ src/stores/utils/addCantoToWallet.ts
@@ -2,28 +2,41 @@ import { ethers } from "ethers";
 import { CantoMain } from "./networks";
 import { CantoMainnet, NodeAddresses } from "cantoui";
 
-export function addNetwork() {
+export async function addNetwork() {
   //@ts-ignore
-  window.ethereum
-    .request({
-      method: "wallet_addEthereumChain",
-      params: [
-        {
-          chainId: "0x" + CantoMain.chainId.toString(16),
-          chainName: "Canto",
-          nativeCurrency: {
-            name: "Canto Coin",
-            symbol: "CANTO",
-            decimals: 18,
-          },
-          rpcUrls: [NodeAddresses.CantoMainnet.ChandraRPC],
-          blockExplorerUrls: [CantoMainnet.blockExplorerUrl],
-        },
-      ],
-    })
-    .catch((error: any) => {
-      console.log(error);
-    });
+  if (window.ethereum) {
+    try {
+      //@ts-ignore
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x1e14" }],
+      });
+    } catch (error: any) {
+      if (error.code === 4902) {
+        //@ts-ignore
+        window?.ethereum
+          .request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0x" + CantoMainnet.chainId.toString(16),
+                chainName: "Canto",
+                nativeCurrency: {
+                  name: "Canto Coin",
+                  symbol: "CANTO",
+                  decimals: 18,
+                },
+                rpcUrls: [NodeAddresses.CantoMainnet.Plex.rpcUrl],
+                blockExplorerUrls: [CantoMainnet.blockExplorerUrl],
+              },
+            ],
+          })
+          .catch((error: any) => {
+            // console.log(error);
+          });
+      }
+    }
+  }
 }
 
 export async function getChainIdandAccount(): Promise<string[] | undefined[]> {
